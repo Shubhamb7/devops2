b40b4a7953bae00069a35b152c1b901289da3cd3
Shubham Belgaonkar <sbelgaonkar@AD.NSEIT.COM>
2023-01-19 21:39:15 +0530

    changes

---
+++ src/test/java/com/spring/testapp/TestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
