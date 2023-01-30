585bb43a637ab6002407b819d58bb3608e6cf4bb
Aashish Kapadne <akapadne@AD.NSEIT.COM>
2023-01-30 15:07:57 +0530

    new

--- src/main/java/com/spring/testapp/controller/TempController.java
+++ src/main/java/com/spring/testapp/controller/TempController.java
@@ -8,7 +8,7 @@ public class TempController {
 
     @GetMapping("/")
     public String getHome(){
-        return "dev website - updatedV4--";
+        return "dev website - updatedV4";
     }
     
 }
