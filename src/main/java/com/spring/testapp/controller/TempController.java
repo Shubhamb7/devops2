38a3afe81e66e29eb6a1fa9b1f52a7884141187a
Shubham Belgaonkar <shubhamb756@gmail.com>
2023-01-30 13:18:48 +0530

    s
    s

--- src/main/java/com/spring/testapp/controller/TempController.java
+++ src/main/java/com/spring/testapp/controller/TempController.java
@@ -8,7 +8,7 @@ public class TempController {
 
     @GetMapping("/")
     public String getHome(){
-        return "aws devops is the best";
+        return "aws devops is the best V2";
     }
     
 }
