827326c6cb4a187a94858b1bd2cd106a0f7f72bc
Shubham Belgaonkar <shubhamb756@gmail.com>
2023-01-30 13:25:06 +0530

    3
    3

--- src/main/java/com/spring/testapp/controller/TempController.java
+++ src/main/java/com/spring/testapp/controller/TempController.java
@@ -8,7 +8,7 @@ public class TempController {
 
     @GetMapping("/")
     public String getHome(){
-        return "dev website - updated";
+        return "dev website - updatedV3";
     }
     
 }
